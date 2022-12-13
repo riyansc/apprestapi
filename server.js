cf5c44264e59b98dc2aa14c63e70e2d6bc6158d6
Riyan Saputra <riyansaputratkj1@gmail.com>
2022-12-13 15:55:05 +0700

    menambahkan file koneksi

---
+++ server.js
@@ -0,0 +1,11 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+// Parse application json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log("Server started on port");
+});
