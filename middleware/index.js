584e13ef8cb2b290f39447db82a4d5eaf4250c3c
Riyan Saputra <riyansaputratkj1@gmail.com>
2022-12-15 16:47:46 +0700

    finish

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var auth = require("./auth");
+const verifikasi = require('./verifikasi');
 var router = express.Router();
 
 // Daftarkan registrasi
 router.post("/api/v1/register", auth.registrasi);
 router.post("/api/v1/login", auth.login);
+
+// Alamat yang perlu autorisasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 module.exports = router;
