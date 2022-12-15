24764e674550f34239f4e9bffc2fd2dd33e474ea
Riyan Saputra <riyansaputratkj1@gmail.com>
2022-12-15 14:19:59 +0700

    membuat post registrasi

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,19 @@
-var connection = require("../koneksi");
-var mysql = require("mysql");
-var md5 = require("md5");
-var response = require("../res");
-var jwt = require("jsonwebtoken");
-var config = require("../config/secret");
-var ip = require("ip");
+var connection = require('../koneksi.js');
+var mysql = require('mysql');
+var md5 = require('MD5');
+var response = require('../res');
+var jwt = require('jsonwebtoken');
+var config = require('../config/secret.js');
+var ip = require('ip');
 
 // * Controller untuk register
-exports.register = function (req, res) {
+exports.registrasi = function (req, res) {
   var post = {
     username: req.body.username,
     email: req.body.email,
     password: md5(req.body.password),
     role: req.body.role,
-    tanggal_daftar: new Date(),
+    tanggal_daftar: new Date()
   };
 
   var query = "SELECT email FROM ?? WHERE ??";
@@ -26,14 +26,14 @@ exports.register = function (req, res) {
       console.log(error);
     } else {
       if (rows.length == 0) {
-        var query = " INSERT INTO ?? SET ?";
+        var query = "INSERT INTO ?? SET ?";
         var table = ["user"];
         query = mysql.format(query, table);
         connection.query(query, post, function (error, rows) {
           if (error) {
             console.log(error);
           } else {
-            response.ok("Berhasil menambahkan user baru");
+            response.ok("Berhasil menambahkan user baru", res);
           }
         });
       } else {
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+var morgan = require("morgan");
 
 // Parse application json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // Panggil routes
 var routes = require("./router");
 routes(app);
 
-app.listen(3000, () => {
+// Daftarkan menu routes dari index
+app.use('/auth', require("./middleware"));
+
+app.listen(3001, () => {
   console.log(`Server started on port 3000`);
 });
